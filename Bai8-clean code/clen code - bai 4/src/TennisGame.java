8d295d0b4c0f9129383fbc0e67f95244ff23e27e
hieple2110 <lequanghiep.21101994@gmail.com>
2020-08-18 10:21:50 +0700

    bai clean code

---
+++ Bai8-clean code/clen code - bai 4/src/TennisGame.java
@@ -0,0 +1,61 @@
+public class TennisGame {
+    public static String getScore(String playName1, String playName2, int play1Score, int play2Score) {
+        String score = "";
+        int tempScore = 0;
+        if (play1Score == play2Score) {
+            switch (play1Score) {
+                case 0:
+                    score = "Love-All";
+                    break;
+                case 1:
+                    score = "Fifteen-All";
+                    break;
+                case 2:
+                    score = "Thirty-All";
+                    break;
+                case 3:
+                    score = "Forty-All";
+                    break;
+                default:
+                    score = "Deuce";
+                    break;
+
+            }
+        } else if (play1Score >= 4 || play2Score >= 4) {
+            int minusResult = play1Score - play2Score;
+            if (minusResult == 1) {
+                score = "Advantage player1";
+            } else if (minusResult == -1) {
+                score = "Advantage player2";
+            } else if (minusResult >= 2) {
+                score = "Win for player1";
+            } else {
+                score = "Win for player2";
+            }
+        } else {
+            for (int i = 1; i < 3; i++) {
+                if (i == 1) {
+                    tempScore = play1Score;
+                } else {
+                    score += "-";
+                    tempScore = play2Score;
+                }
+                switch (tempScore) {
+                    case 0:
+                        score += "Love";
+                        break;
+                    case 1:
+                        score += "Fifteen";
+                        break;
+                    case 2:
+                        score += "Thirty";
+                        break;
+                    case 3:
+                        score += "Forty";
+                        break;
+                }
+            }
+        }
+        return score;
+    }
+}
